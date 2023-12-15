7726eacfb66fa1ff0e202819e8e8eadc4a0bc0a0
AliKhatami <khatamiiiuc49@gmail.com>
2023-12-15 14:41:04 +0600

    khatami

---
+++ akhello.js
@@ -0,0 +1,30 @@
+// in line comments
+
+/*My name is 
+ali  khatami
+I am a student 
+*/
+
+/*num=9;
+
+var num = "cat";
+ num = 18;
+
+ console.log(num);
+
+ let car = "boy";
+
+ car= 12;
+
+ console.log(car);
+*/
+
+var n =10
+
+var mm = Number('68.89')
+
+console.log(mm)
+
+console.log(Number.parseInt(mm))
+
+ 
---
+++ start.js
@@ -0,0 +1,24 @@
+// in line comments
+
+/*My name is 
+ali  khatami
+I am a student 
+
+
+
+*/
+
+num=9;
+
+var num = "cat";
+ num = 18;
+
+ console.log(num);
+
+ let car = "boy";
+
+ car= 12;
+
+ console.log(car);
+
+ console.log()
